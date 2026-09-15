2d9a4578c97b1a8d7e67c338f3f70d991523fecf
Thaduri-Sairamya <sairamyathaduri7@gmail.com>
2026-09-15 17:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Test App")
-
-st.title("Job Salary Prediction")
-
-st.write("Streamlit application is running successfully.")
-
-st.success("Test successful!")
+st.title("Hello")
+st.write("My Streamlit app is working!")
